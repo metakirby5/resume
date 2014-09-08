f211afeebe5fee1d723246b9dd66d0e364a9bcc5
Ethan Chan <metakirby5@gmail.com>
2014-09-08 03:24:56 -0700

    reindent

--- js/main.js
+++ js/main.js
@@ -1,247 +1,247 @@
 (function($, _, Transparency) {
 
-  "use strict";
+"use strict";
 
-  // # Constants
-  var XS = 0,
-      SM = 768,
-      MD = 992,
-      LG = 1200,
-      VIDEO_ID = 'zKdwTgrow3E',
-      EMBED = '<iframe style="position:fixed" width="1" height="1" frameborder="0" src="//www.youtube.com/v/' + VIDEO_ID + '?hd=1&autoplay=1&loop=1&playlist=,"></iframe>',
-      SECRET = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+// # Constants
+var XS = 0,
+    SM = 768,
+    MD = 992,
+    LG = 1200,
+    VIDEO_ID = 'zKdwTgrow3E',
+    EMBED = '<iframe style="position:fixed" width="1" height="1" frameborder="0" src="//www.youtube.com/v/' + VIDEO_ID + '?hd=1&autoplay=1&loop=1&playlist=,"></iframe>',
+    SECRET = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-  // # Globals
-  var kkeys = [];
+// # Globals
+var kkeys = [];
 
-  // # Utilities
-  var pLog = _.curry(function(qty, text, args) {
-    console.log.apply(console, [text + ': '].concat([].slice.call(arguments, 2, qty + 2)));
-  });
-  var getIdx = _.curry(function(idx, arr) {return arr[idx];});
+// # Utilities
+var pLog = _.curry(function(qty, text, args) {
+  console.log.apply(console, [text + ': '].concat([].slice.call(arguments, 2, qty + 2)));
+});
+var getIdx = _.curry(function(idx, arr) {return arr[idx];});
 
-  // Takes object with width:func(curWidth)
-  function onWidths(obj) {
-    // Initialize 0 if not already there
-    if (!obj[0])
-      obj[0] = _.noop;
+// Takes object with width:func(curWidth)
+function onWidths(obj) {
+  // Initialize 0 if not already there
+  if (!obj[0])
+    obj[0] = _.noop;
 
-    var base = function() {
-      var $width = window.outerWidth;
-      var reduction = function(a, v, k) {return $width >= +k && +k > +a ? +k : +a;};
-      obj[_.reduce(obj, reduction, 0)]($width);
-    };
-
-    $(base);
-    $(window).resize(base);
-  }
-
-  function errRedirect(jqXHR, msg, err) {
-    console.log(msg, err);
-    if (document.location.hostname !== "localhost")
-      window.location.replace('error.html?msg='+msg+'&err='+err);
-  }
-
-  // Make transparency only match data-bind and data-cmd
-  Transparency.matcher = function(element, key) {
-    return (element.el.getAttribute('data-bind') === key ||
-            element.el.getAttribute('data-cmd') === key);
+  var base = function() {
+    var $width = window.outerWidth;
+    var reduction = function(a, v, k) {return $width >= +k && +k > +a ? +k : +a;};
+    obj[_.reduce(obj, reduction, 0)]($width);
   };
 
-  // # Directive helpers
-  var hideIfNot = _.curry(function(key, p) {
-    var req = p.element.dataset[key];
-    if (!req)
-      return;
+  $(base);
+  $(window).resize(base);
+}
 
-    req = req.split(' ');
-    var thiz = this;
-    var reduction = function(a, b) {return a && !thiz[b];};
+function errRedirect(jqXHR, msg, err) {
+  console.log(msg, err);
+  if (document.location.hostname !== "localhost")
+    window.location.replace('error.html?msg='+msg+'&err='+err);
+}
 
-    if (_(req).reduce(reduction, true))
-      return 'display: none';
-  });
+// Make transparency only match data-bind and data-cmd
+Transparency.matcher = function(element, key) {
+  return (element.el.getAttribute('data-bind') === key ||
+          element.el.getAttribute('data-cmd') === key);
+};
 
-  var addClasses = _.curry(function(classes, p) {
-    return p.element.className + ' ' + classes.join(' ');
-  });
+// # Directive helpers
+var hideIfNot = _.curry(function(key, p) {
+  var req = p.element.dataset[key];
+  if (!req)
+    return;
 
-  var hiddenPrint = function(p) {
-    if (this.hiddenPrint)
-      return addClasses(['hidden-print'], p);
-  };
+  req = req.split(' ');
+  var thiz = this;
+  var reduction = function(a, b) {return a && !thiz[b];};
 
-  var noText = function() {
-    return '';
-  };
+  if (_(req).reduce(reduction, true))
+    return 'display: none';
+});
 
-  var targetBlank = function() {
-    return '_blank';
-  };
+var addClasses = _.curry(function(classes, p) {
+  return p.element.className + ' ' + classes.join(' ');
+});
 
-  var defaultBGI = function() {
-    return 'background-image: url(' + (this.image || 'img/default.png') + ')';
-  };
+var hiddenPrint = function(p) {
+  if (this.hiddenPrint)
+    return addClasses(['hidden-print'], p);
+};
 
-  var webLink = function(p) {
-    // Get dest obj from data binding
-    var dest = p.element.dataset.bind;
-    if (!dest || !this[dest])
-      return;
-    var url = this[dest];
+var noText = function() {
+  return '';
+};
 
-    // Add protocol if not already at start
-    if (url.indexOf('http://') && url.indexOf('https://'))
-      url = 'http://' + url;
-    return url;
-  };
+var targetBlank = function() {
+  return '_blank';
+};
 
-  var namedWebLink = function() {
-    // Requires url to be in values scope
-    var url = this.url;
-    if (!url)
-      return;
+var defaultBGI = function() {
+  return 'background-image: url(' + (this.image || 'img/default.png') + ')';
+};
 
-    // Add protocol if not already at start
-    if (url.indexOf('http://') && url.indexOf('https://'))
-      url = 'http://' + url;
-    return url;
-  };
+var webLink = function(p) {
+  // Get dest obj from data binding
+  var dest = p.element.dataset.bind;
+  if (!dest || !this[dest])
+    return;
+  var url = this[dest];
 
-  // # Directive blocks
-  var card = {
-    container: {
-      class: hiddenPrint
-    },
-    name: {
-      href: namedWebLink,
-      target: targetBlank
-    },
-    content: {
-      value: {
-        text: function() {
-          return this.value;
-        }
-      },
-      style: hideIfNot('bind')
-    },
-    url: {
-      text: noText,
-      href: webLink,
-      target: targetBlank
-    },
-    image: {
-      text: noText,
-      style: defaultBGI
-    }
-  };
+  // Add protocol if not already at start
+  if (url.indexOf('http://') && url.indexOf('https://'))
+    url = 'http://' + url;
+  return url;
+};
 
-  $(function() {
-    // Fade in after ajax
-    $('body').hide();
+var namedWebLink = function() {
+  // Requires url to be in values scope
+  var url = this.url;
+  if (!url)
+    return;
 
-    // Ajax calls
-    $.when(
-      $.getJSON('data/data.json', function(data) {
-        $('html').render(data, {
-          'hide-if-not': {
-            text: noText,
-            style: hideIfNot('values')
-          },
-          'hide-if-not-bound': {
-            text: noText,
-            style: hideIfNot('bind')
-          },
-          fullname: {
-            text: function() {
-              return [this.first, this.last].join(' ');
-            }
-          },
-          phone: {
-            href: function() {
-              if (this.phone)
-                return 'tel:' + this.phone.replace(/\s+/g, '');
-            }
-          },
-          email: {
-            href: function() {
-              if (this.email)
-                return 'mailto:' + this.email;
-            }
-          },
-          location: {
-            href: function() {
-              if (this.location)
-                return 'https://www.google.com/maps/place/' + this.location.replace(/\s+/g, '+');
-            },
-            target: targetBlank
-          },
-          link: {
-            href: webLink,
-            target: targetBlank
-          },
-          experience: card,
-          skills: {
-            icon: {
-              text: noText,
-              class: function(p) {
-                return p.element.className + ' ' + (this.icon || 'fa-code');
-              }
-            }
-          },
-          projects: card,
-          organizations: {
-            namedate: {
-              text: function(p) {
-                return this.name + (this.date ? ' (' + this.date + ')' : '');
-              }
-            }
-          }
-        });
-      }).fail(errRedirect),
+  // Add protocol if not already at start
+  if (url.indexOf('http://') && url.indexOf('https://'))
+    url = 'http://' + url;
+  return url;
+};
 
-      $.getJSON('data/credits.json', function(data) {
-        $('#credits-list').render(data, {
-          entry: {
-            text: function() {
-              return this.name;
-            },
-            href: namedWebLink,
-            target: targetBlank
-          }
-        });
-      }).fail(errRedirect)
-    ).
-
-    then(function() {
-      var reduction = function(a, b) {return a && (b === 'success');};
-
-      if (_(arguments).map(getIdx(1)).reduce(reduction, true))
-        $('body').fadeIn('slow');
-      else
-        errRedirect(null, 'Not all ajax calls returned success', arguments);
-    });
-
-    // Other listeners
-    var widths = {};
-    var pLog1 = pLog(1);
-    widths[XS] = pLog1('XS');
-    widths[SM] = pLog1('SM');
-    widths[MD] = pLog1('MD');
-    widths[LG] = pLog1('LG');
-    onWidths(widths);
-
-    // shhhh
-    var secretFunc;
-    $(document).keydown(secretFunc = function(e) {
-      kkeys.push(e.keyCode);
-      if (kkeys.length > SECRET.length)
-        kkeys.shift();
-      if (_.isEqual(kkeys, SECRET)) {
-        $(document).unbind('keydown', secretFunc);
-        $('body').append(EMBED);
+// # Directive blocks
+var card = {
+  container: {
+    class: hiddenPrint
+  },
+  name: {
+    href: namedWebLink,
+    target: targetBlank
+  },
+  content: {
+    value: {
+      text: function() {
+        return this.value;
       }
-    });
+    },
+    style: hideIfNot('bind')
+  },
+  url: {
+    text: noText,
+    href: webLink,
+    target: targetBlank
+  },
+  image: {
+    text: noText,
+    style: defaultBGI
+  }
+};
+
+$(function() {
+  // Fade in after ajax
+  $('body').hide();
+
+  // Ajax calls
+  $.when(
+    $.getJSON('data/data.json', function(data) {
+      $('html').render(data, {
+        'hide-if-not': {
+          text: noText,
+          style: hideIfNot('values')
+        },
+        'hide-if-not-bound': {
+          text: noText,
+          style: hideIfNot('bind')
+        },
+        fullname: {
+          text: function() {
+            return [this.first, this.last].join(' ');
+          }
+        },
+        phone: {
+          href: function() {
+            if (this.phone)
+              return 'tel:' + this.phone.replace(/\s+/g, '');
+          }
+        },
+        email: {
+          href: function() {
+            if (this.email)
+              return 'mailto:' + this.email;
+          }
+        },
+        location: {
+          href: function() {
+            if (this.location)
+              return 'https://www.google.com/maps/place/' + this.location.replace(/\s+/g, '+');
+          },
+          target: targetBlank
+        },
+        link: {
+          href: webLink,
+          target: targetBlank
+        },
+        experience: card,
+        skills: {
+          icon: {
+            text: noText,
+            class: function(p) {
+              return p.element.className + ' ' + (this.icon || 'fa-code');
+            }
+          }
+        },
+        projects: card,
+        organizations: {
+          namedate: {
+            text: function(p) {
+              return this.name + (this.date ? ' (' + this.date + ')' : '');
+            }
+          }
+        }
+      });
+    }).fail(errRedirect),
+
+    $.getJSON('data/credits.json', function(data) {
+      $('#credits-list').render(data, {
+        entry: {
+          text: function() {
+            return this.name;
+          },
+          href: namedWebLink,
+          target: targetBlank
+        }
+      });
+    }).fail(errRedirect)
+  ).
+
+  then(function() {
+    var reduction = function(a, b) {return a && (b === 'success');};
+
+    if (_(arguments).map(getIdx(1)).reduce(reduction, true))
+      $('body').fadeIn('slow');
+    else
+      errRedirect(null, 'Not all ajax calls returned success', arguments);
   });
+
+  // Other listeners
+  var widths = {};
+  var pLog1 = pLog(1);
+  widths[XS] = pLog1('XS');
+  widths[SM] = pLog1('SM');
+  widths[MD] = pLog1('MD');
+  widths[LG] = pLog1('LG');
+  onWidths(widths);
+
+  // shhhh
+  var secretFunc;
+  $(document).keydown(secretFunc = function(e) {
+    kkeys.push(e.keyCode);
+    if (kkeys.length > SECRET.length)
+      kkeys.shift();
+    if (_.isEqual(kkeys, SECRET)) {
+      $(document).unbind('keydown', secretFunc);
+      $('body').append(EMBED);
+    }
+  });
+});
 
 })(window.jQuery, window._, window.Transparency);
